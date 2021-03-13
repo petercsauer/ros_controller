b1b5f794b6b99ae4ff0fb6552ee98a877ffcfc74
Peter Sauer <petercsauer@me.com>
2021-03-12 19:30:20 -0600

    initial commit

---
+++ src/controller.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+from pynput.mouse import Button, Controller
+import qwiic_joystick
+import pygame
+import time
+import sys
+import rospy
+from geometry_msgs.msg import Twist 
+
+# import interface
+
+class Control:
+    def __init__(self):
+        self.joy1 = qwiic_joystick.QwiicJoystick()
+        self.horizontal1 = 0.0
+        self.vertical1 = 0.0
+        self.button1 = False
+        self.mouse = Controller()
+        # self.cmd_pub = rospy.Publisher("cmd_vel", Twist, queue_size=0)
+        self.timer = rospy.Time.now()
+        self.mode = 1
+
+
+    def updateJoystickValues(self):
+        self.joy1.begin()
+        self.horizontal1 = self.joy1.vertical
+        self.vertical1 = self.joy1.horizontal
+        self.button1 = self.joy1.button
+
+    def controlMouse(self):
+        speed = 100
+        if self.joy1.connected:
+            self.joy1.begin()
+            if not self.joy1.button:
+                print("BUTTON")
+                self.mouse.click(Button.left, 1)
+                time.sleep(0.5)
+            self.mouse.move(self.joy1.vertical/speed-5.18, -self.joy1.horizontal/speed+5.01)
+
+
+    def controlRobot(self):
+        speed = 100
+    #     if self.joy1.connected:
+    #         x_dot = self.joy.vertical/speed-5.18
+    #         omega = -self.joy.horizontal/speed+5.01
+    #         twist = Twist(linear = Vector3(x = xdot, y = 0, z = 0), angular = Vector3(x = 0, y = 0, z = omega))
+    #         self.cmd_pub.publish(twist)
+
+    def checkControl(self):
+        if not self.joy1.button:
+            if (rospy.Time.now()-self.timer).to_sec() > 2:
+                if self.mode == 0:
+                    self.mode = 1            
+                    self.timer = rospy.Time.now()
+
+                else:
+                    self.mode = 0
+                    self.timer = rospy.Time.now()
+
+        else:
+            self.timer = rospy.Time.now()
+
+
+
+
+pygame.init()
+
+# interface = Interface(pygame)
+
+if __name__ == '__main__':
+    rospy.init_node("controller")
+    c = Control()
+    while not rospy.is_shutdown():
+        print(c.mode)
+        c.checkControl()
+        if c.mode:
+            c.controlMouse()
+        else:
+            c.controlRobot()
+    rospy.spin()
+    
+            
---
+++ src/interface.py
@@ -0,0 +1,222 @@
+import sys, pygame
+import pygame.gfxdraw
+import pygame.freetype
+from pygame_vkeyboard import *
+
+
+COLOR_INACTIVE = pygame.Color('lightskyblue3')
+COLOR_ACTIVE = pygame.Color('dodgerblue2')
+
+
+def draw_rounded_rect(surface, rect, color, corner_radius):
+    ''' Draw a rectangle with rounded corners.
+    Would prefer this: 
+        pygame.draw.rect(surface, color, rect, border_radius=corner_radius)
+    but this option is not yet supported in my version of pygame so do it ourselves.
+
+    We use anti-aliased circles to make the corners smoother
+    '''
+    if rect.width < 2 * corner_radius or rect.height < 2 * corner_radius:
+        raise ValueError(f"Both height (rect.height) and width (rect.width) must be > 2 * corner radius ({corner_radius})")
+
+    # need to use anti aliasing circle drawing routines to smooth the corners
+    pygame.gfxdraw.aacircle(surface, rect.left+corner_radius, rect.top+corner_radius, corner_radius, color)
+    pygame.gfxdraw.aacircle(surface, rect.right-corner_radius-1, rect.top+corner_radius, corner_radius, color)
+    pygame.gfxdraw.aacircle(surface, rect.left+corner_radius, rect.bottom-corner_radius-1, corner_radius, color)
+    pygame.gfxdraw.aacircle(surface, rect.right-corner_radius-1, rect.bottom-corner_radius-1, corner_radius, color)
+
+    pygame.gfxdraw.filled_circle(surface, rect.left+corner_radius, rect.top+corner_radius, corner_radius, color)
+    pygame.gfxdraw.filled_circle(surface, rect.right-corner_radius-1, rect.top+corner_radius, corner_radius, color)
+    pygame.gfxdraw.filled_circle(surface, rect.left+corner_radius, rect.bottom-corner_radius-1, corner_radius, color)
+    pygame.gfxdraw.filled_circle(surface, rect.right-corner_radius-1, rect.bottom-corner_radius-1, corner_radius, color)
+
+    rect_tmp = pygame.Rect(rect)
+
+    rect_tmp.width -= 2 * corner_radius
+    rect_tmp.center = rect.center
+    pygame.draw.rect(surface, color, rect_tmp)
+
+    rect_tmp.width = rect.width
+    rect_tmp.height -= 2 * corner_radius
+    rect_tmp.center = rect.center
+    pygame.draw.rect(surface, color, rect_tmp)
+
+class Button:
+    def __init__(self, surface, rect, text):
+        self.surface = surface
+        self.rect = rect
+        self.text = text
+
+        circular14 = pygame.freetype.Font("CircularStd-Bold.ttf",14)
+        p_rect = pygame.rect.Rect(rect)
+        draw_rounded_rect(surface, p_rect, (200,200,200),18)
+
+    def render_button(self, mouse):
+        circular14 = pygame.freetype.Font("CircularStd-Bold.ttf",14)
+
+        p_rect = pygame.rect.Rect(self.rect)
+        if mouse[0] < self.rect[0]+self.rect[2] and mouse[0]> self.rect[0] and mouse[1] < self.rect[1]+self.rect[3] and mouse[1] > self.rect[1]:
+            draw_rounded_rect(self.surface, p_rect, (255,255,255),18)
+        else:
+            draw_rounded_rect(self.surface, p_rect, (200,200,200),18)
+
+        circular14.render_to(self.surface, (self.rect[0]+10, self.rect[1]+15), self.text, (0,0,0))
+    
+    def check_click(self, mouse):
+        if mouse[0] < self.rect[0]+self.rect[2] and mouse[0]> self.rect[0] and mouse[1] < self.rect[1]+self.rect[3] and mouse[1] > self.rect[1]:
+            return 1
+        else:
+            return 0
+    
+class IconButton:
+    def __init__(self, surface, pos, radius, icon):
+        self.surface = surface
+        self.pos = pos
+        self.icon = pygame.transform.scale(pygame.image.load(icon), (radius, radius) )
+
+        self.radius = radius
+
+    def render_button(self, mouse):
+        if mouse[0] < self.pos[0]+self.radius and mouse[0]> self.pos[0]-self.radius and mouse[1] < self.pos[1]+self.radius and mouse[1] > self.pos[1] - self.radius:
+            pygame.draw.circle(self.surface, (200,200,200), self.pos, self.radius)
+            self.surface.blit(self.icon, (self.pos[0]-self.radius/2.4, self.pos[1]-self.radius/2)) 
+
+        else:
+            pygame.draw.circle(self.surface, (255,255,255), self.pos, self.radius)
+            self.surface.blit(self.icon, (self.pos[0]-self.radius/2.4, self.pos[1]-self.radius/2)) 
+
+    
+    def check_click(self, mouse):
+        if mouse[0] < self.pos[0]+self.radius and mouse[0]> self.pos[0]-self.radius and mouse[1] < self.pos[1]+self.radius and mouse[1] > self.pos[1] - self.radius:
+            return 1
+        else:
+            return 0
+
+
+class ServiceMenuItem:
+    def __init__(self, surface, id, package, service, interface):
+        self.w = 310
+        self.h = 80
+        self.package = package
+        self.service = service
+        self.surface = surface
+        self.id = id
+        self.rect = pygame.rect.Rect(40,100+100*id,self.w,self.h)
+        self.button = IconButton(surface, (self.w+10,int(self.h/2)+100+100*id), 20, "play.png")
+        self.submit = 0
+        layout = VKeyboardLayout(VKeyboardLayout.QWERTY)
+        self.keyboard = VKeyboard(self.surface, self.consumer, layout)
+        self.window = False
+        
+    def render(self, mouse, events):
+        if mouse[0] < self.rect[0]+self.rect[2] and mouse[0]> self.rect[0] and mouse[1] < self.rect[1]+self.rect[3] and mouse[1] > self.rect[1]:
+            draw_rounded_rect(self.surface, self.rect, (190,190,190),15)
+        else:
+            draw_rounded_rect(self.surface, self.rect, (230,230,230),15)
+
+        self.circular16 = pygame.freetype.Font("CircularStd-Bold.ttf",20)
+        self.circular12 = pygame.freetype.Font("CircularStd-Bold.ttf",16)        
+        self.circular12.render_to(self.surface, (60, 123+100*self.id), self.package, (50,50,50))        
+        self.circular16.render_to(self.surface, (60, 142+100*self.id), self.service, (0,0,0))
+        self.button.render_button(mouse)
+        self.keyboard.update(events)
+        if self.window:
+            self.keyboard.draw(self.surface)
+
+
+    def check_click(self, mouse):
+
+        if self.button.check_click(mouse):
+            return 2
+        elif mouse[0] < self.rect[0]+self.rect[2] and mouse[0]> self.rect[0] and mouse[1] < self.rect[1]+self.rect[3] and mouse[1] > self.rect[1]:
+            self.popup()
+            return 1
+        else:
+            return 0
+
+    def popup(self):
+        self.window=True
+        width = 800
+        height = 600
+        rect = pygame.rect.Rect((1872-width)/2, (1000-height)/2, width, height/2)
+        draw_rounded_rect(self.surface,rect,(200,200,200), 15)
+        
+        txt_rect = pygame.rect.Rect((1872-width)/2+20, (1000-height)/2+100, width-40, 35)
+        draw_rounded_rect(self.surface, txt_rect, (255,255,255),15)
+
+        self.circular12.render_to(self.surface, ((1872-width)/2+20, (1000-height)/2+20), self.package, (50,50,50))        
+        self.circular16.render_to(self.surface, ((1872-width)/2+20, (1000-height)/2+40), self.service, (0,0,0))
+
+        submit_rect = pygame.rect.Rect((1872-800)/2+600, (1000-600)/2+235, 800-620, 45)
+        self.submit = Button(self.surface, submit_rect,"Submit")
+        # self.submit.render_button(mouse)
+            
+    def consumer(self, text):
+        width = 800
+        height = 600
+        txt_rect = pygame.rect.Rect((1872-width)/2+20, (1000-height)/2+100, width-40, 35)
+        draw_rounded_rect(self.surface, txt_rect, (255,255,255),15)
+        self.circular12.render_to(self.surface, ((1872-width)/2+30, (1000-height)/2+110), text, (50,50,50))    
+    
+class Interface:
+    def __init__(self, pygame):
+        
+        self.pygame = pygame
+        self.pygame.font.init()
+        self.circular20 = self.pygame.freetype.Font("CircularStd-Bold.ttf",28)
+        FONT= self.pygame.freetype.Font("CircularStd-Bold.ttf",14)
+
+        self.size = self.width, self.height = 1872, 1404
+        speed = [2, 2]
+        self.BLACK = (0, 0, 0)
+        self.WHITE = (255,255,255)
+
+        self.screen = pygame.display.set_mode(self.size)
+
+        self.menu = []
+
+        self.reset()
+
+
+    def reset(self):
+
+        srv_rect = self.pygame.rect.Rect(20,20,350,self.height-40)
+        console_rect = self.pygame.rect.Rect(810,20,self.width-810-20,self.height-40)
+        cntrl_rect = self.pygame.rect.Rect(390,20,400,self.height-40)
+
+        self.screen.fill(self.WHITE)
+        draw_rounded_rect(self.screen, srv_rect, self.BLACK, 20)
+        draw_rounded_rect(self.screen, cntrl_rect, self.BLACK, 20)
+        draw_rounded_rect(self.screen, console_rect, self.BLACK, 20)
+
+        self.circular20.render_to(self.screen, (50,50), "ROS Services", self.WHITE)
+        self.circular20.render_to(self.screen, (835,50), "Console", self.WHITE)
+        self.circular20.render_to(self.screen, (415,50), "Basic Controls", self.WHITE)
+
+        self.menu.append(ServiceMenuItem(self.screen, 0, "turtlesim", "translate", self))
+        self.menu.append(ServiceMenuItem(self.screen, 1, "controller", "forward", self))
+        self.menu.append(ServiceMenuItem(self.screen, 2, "controller", "backward", self))
+
+    def refresh(self):
+        mouse = self.pygame.mouse.get_pos() 
+        events = self.pygame.event.get()
+        for m in self.menu:
+            m.render(mouse, events)
+        for event in events:
+            if event.type==self.pygame.QUIT:
+                self.pygame.quit()
+                sys.exit()
+            if event.type == self.pygame.MOUSEBUTTONDOWN: 
+                for m in self.menu:
+                    m.check_click(mouse)
+
+
+        self.pygame.display.update()
+
+
+pygame.init()
+
+interface = Interface(pygame)
+
+while True:
+        interface.refresh()
